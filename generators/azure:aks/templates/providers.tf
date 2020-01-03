78ebdddae41de600f33e0139ae08c181b3daf086
Sergio Sisternes <sesispla@outlook.com>
2020-01-03 20:44:00 +0100

    Fixed provider error

--- generators/azure:aks/templates/providers.tf
+++ generators/azure:aks/templates/providers.tf
@@ -5,8 +5,6 @@ provider "azurerm" {
 provider "kubernetes" {
   version                = "1.10"
   host                   = module.<%= name %>-aks.kube_config.0.host
-  username               = module.<%= name %>-aks.kube_config.0.username
-  password               = module.<%= name %>-aks.kube_config.0.password
   client_certificate     = base64decode(module.<%= name %>-aks.kube_config.0.client_certificate)
   client_key             = base64decode(module.<%= name %>-aks.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(module.<%= name %>-aks.kube_config.0.cluster_ca_certificate)
@@ -17,8 +15,6 @@ provider "helm" {
   service_account  = "tiller" // For helm v2.0
   kubernetes {
     host                   = module.<%= name %>-aks.kube_config.0.host
-    username               = module.<%= name %>-aks.kube_config.0.username
-    password               = module.<%= name %>-aks.kube_config.0.password
     client_certificate     = base64decode(module.<%= name %>-aks.kube_config.0.client_certificate)
     client_key             = base64decode(module.<%= name %>-aks.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(module.<%= name %>-aks.kube_config.0.cluster_ca_certificate)
