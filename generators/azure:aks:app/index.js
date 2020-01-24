57018765fc2325ce45b80cdd6cbb671ec32c7aac
Sergio Sisternes <sesispla@outlook.com>
2020-01-24 23:29:24 +0000

    Added information to ensure the SP has the appropiate permissions

--- generators/azure:aks:app/index.js
+++ generators/azure:aks:app/index.js
@@ -37,6 +37,12 @@ module.exports = class extends Generator {
   }
 
   end() {
+    if (this.answers.features.exists('azure-network')) {
+      this.log(`IMPORTANT: When using Azure CNI, AKS Service Principal '${this.answers.clientId}' should have 'Network Contributor' permissions at '${this.answers.aksResourceGroup}' in order to Ensure Load Balancers.`);
+      this.log(`If you are experiencing issues with Load Balancers, please check with your Azure Administrator the Service Principal has the appropiate permissions.`);
+      this.log("More information: https://github.com/Azure/AKS/issues/357#issuecomment-394583518")
+    }    
+    
     this.config.set(this.configName, this.answers);
     this.config.save();
   }
