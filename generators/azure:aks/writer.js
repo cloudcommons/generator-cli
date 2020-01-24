1034d8189a554c7d0b6bc71240bf412f0bc7d873
Sergio Sisternes <sesispla@outlook.com>
2020-01-24 19:59:14 +0000

    Added atarraya

--- generators/azure:aks/writer.js
+++ generators/azure:aks/writer.js
@@ -24,6 +24,9 @@ module.exports = function (generator, answers) {
         fsTools.copyTo(generator, `cert-manager/${answers.certManagerVersion}/jetstack-helm-repo.tf`, "cert-manager-jetstack-helm-repo.tf", answers);
         fsTools.copyTo(generator, `cert-manager/${answers.certManagerVersion}/cert-manager.tf`, "cert-manager.tf", answers);
     }
+    if (answers.features.includes("atarraya")) {
+        fsTools.copyTo(generator, "atarraya/atarraya.tf", "atarraya.tf", answers);
+    }
     if (true) // Future condition to copy Tiller-related rubbish when people use Helm v2
     {
         fsTools.copyTo(generator, "helm/v2/cluster-role.tf", "tiller-cluster-role.tf");
