d9c5f83f668262073c5a736c4520aa47082564a2
Sergio Sisternes <sesispla@outlook.com>
2020-02-01 12:21:35 +0000

    Added remote and azurerm tests

--- test/terraform.js
+++ test/terraform.js
@@ -5,7 +5,7 @@ var terraform = require('../common/terraform');
 const { spawn } = require('child_process');
 
 describe("cloudcommons/cli:terraform", function () {
-    describe('Initialises a local provider', () => {
+    describe('Initialises local provider', () => {
         var prompts = {
             app: 'cloudcommons',
             version: '~> v0.12.19',
@@ -40,4 +40,97 @@ describe("cloudcommons/cli:terraform", function () {
             assert.fileContent('variables.tf.json', `"default": "${prompts.app}"`);      
         });
     });
+
+    describe('Initialises remote provider', () => {
+        var prompts = {
+            app: 'cloudcommons',
+            version: '~> v0.12.19',
+            backendType: 'remote',
+            remoteHostname: 'app.terraform.io',
+            remoteOrganization: 'cloudcommons',
+            remoteWorkspace: 'cli'
+        };
+
+        before(done => {
+            helpers
+                .run(path.join(__dirname, '../generators/terraform'))
+                .withPrompts(prompts)
+                .once('end', done);
+        });
+
+        it('Generates required files', () => {
+            assert.file('LICENSE');
+            assert.file('.gitignore');
+            assert.file('__init__.tf');
+            assert.file('variables.tf.json');
+        });
+
+        it('Is licensed to me :)', () => {
+            assert.fileContent('LICENSE', 'Sergio Sisternes Pla');
+        });
+
+        it('Init file is OK', () => {
+            assert.fileContent('__init__.tf', `required_version = "${prompts.version}"`);
+            assert.fileContent('__init__.tf', `backend "${prompts.backendType}"`);
+            assert.fileContent('__init__.tf', `hostname`);
+            assert.fileContent('__init__.tf', `= "${prompts.remoteHostname}"`);
+            assert.fileContent('__init__.tf', `organization`);
+            assert.fileContent('__init__.tf', `= "${prompts.remoteOrganization}"`);
+        });
+
+        it('Copies the variables', () => {
+            assert.fileContent('variables.tf.json', `"APP": `);
+            assert.fileContent('variables.tf.json', `"default": "${prompts.app}"`);      
+        });
+    }); 
+    
+    describe('Initialises azurerm provider', () => {
+        return it.skip('This test requires mocking the az client');
+        
+        var prompts = {
+            app: 'cloudcommons',
+            version: '~> v0.12.19',
+            backendType: 'azurerm',
+            azureRmResourceGroup: 'terraform',
+            azureRmStorageAccountName: 'terraform01234',
+            azureRmContainerName: 'tfstates',
+            azureRmContainerKey: 'myapp'
+        };
+
+        before(done => {
+            helpers
+                .run(path.join(__dirname, '../generators/terraform'))
+                .withPrompts(prompts)
+                .once('end', done);
+        });
+
+        it('Generates required files', () => {
+            assert.file('LICENSE');
+            assert.file('.gitignore');
+            assert.file('__init__.tf');
+            assert.file('variables.tf.json');
+        });
+
+        it('Is licensed to me :)', () => {
+            assert.fileContent('LICENSE', 'Sergio Sisternes Pla');
+        });
+
+        it('Init file is OK', () => {
+            assert.fileContent('__init__.tf', `required_version = "${prompts.version}"`);
+            assert.fileContent('__init__.tf', `backend "${prompts.backendType}"`);
+            assert.fileContent('__init__.tf', `resource_group_name`);
+            assert.fileContent('__init__.tf', `= "${prompts.azureRmResourceGroup}"`);
+            assert.fileContent('__init__.tf', `storage_account_name`);
+            assert.fileContent('__init__.tf', `= "${prompts.azureRmStorageAccountName}"`);
+            assert.fileContent('__init__.tf', `container_name`);
+            assert.fileContent('__init__.tf', `= "${prompts.azureRmContainerName}"`);
+            assert.fileContent('__init__.tf', `key`);
+            assert.fileContent('__init__.tf', `= "${prompts.azureRmContainerKey}"`);                        
+        });
+
+        it('Copies the variables', () => {
+            assert.fileContent('variables.tf.json', `"APP": `);
+            assert.fileContent('variables.tf.json', `"default": "${prompts.app}"`);      
+        });
+    });        
 });
