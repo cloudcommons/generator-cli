7a9950daeab3e6412ee7f0a827ced85199b23d22
Sergio Sisternes <sesispla@outlook.com>
2020-02-02 11:40:05 +0000

    Removed all role assignments. They should be done manually

--- generators/azure-aks/templates/aks.tf
+++ generators/azure-aks/templates/aks.tf
@@ -4,7 +4,7 @@ locals {
 
 module "<%= name %>-kubernetes" {
   source                    = "cloudcommons/kubernetes/azure"
-  version                   = "0.1.5"
+  version                   = "0.1.6"
   name                      = local.aks_name
   location                  = var.LOCATION
   resource_group            = <%= resourceGroupReference %>
