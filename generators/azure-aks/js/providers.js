469152bc8f7692f2157a2b028fca5673399ca21f
Sergio Sisternes <sesispla@outlook.com>
2020-02-16 16:06:21 +0000

    Changed load_config_file for both Kubernetes and Helm

--- generators/azure-aks/js/providers.js
+++ generators/azure-aks/js/providers.js
@@ -12,7 +12,8 @@ module.exports = {
                     version: "~> 1.4"
                 },
                 kubernetes: {
-                    version: "~> 1.10",
+                    version: "~> 1.11",
+                    load_config_file: false,
                     host: terraform.toVariable(`module.${answers.name}-kubernetes.kube_config.0.host`),
                     client_certificate: terraform.toVariable(`base64decode(module.${answers.name}-kubernetes.kube_config.0.client_certificate)`),
                     client_key: terraform.toVariable(`base64decode(module.${answers.name}-kubernetes.kube_config.0.client_key)`),
--- generators/azure-aks/templates/helm/v3/providers.js
+++ generators/azure-aks/templates/helm/v3/providers.js
@@ -5,6 +5,7 @@ module.exports = {
                 helm: {
                     version: "~> 1.0.0",
                     kubernetes: {
+                        load_config_file: false,
                         host: terraform.toVariable(`module.${answers.name}-kubernetes.kube_config.0.host`),
                         client_certificate: terraform.toVariable(`base64decode(module.${answers.name}-kubernetes.kube_config.0.client_certificate)`),
                         client_key: terraform.toVariable(`base64decode(module.${answers.name}-kubernetes.kube_config.0.client_key)`),
