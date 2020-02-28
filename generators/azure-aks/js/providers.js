22180716c93e6338a3e762e33221f1f208030341
Sergio Sisternes <sesispla@outlook.com>
2020-02-28 17:13:23 +0000

    Using the new providers central class

--- generators/azure-aks/js/providers.js
+++ generators/azure-aks/js/providers.js
@@ -1,24 +1,19 @@
+const coreProviders = require('../../../core/providers');
+
 module.exports = {
     copy: function (terraform, answers) {
         var providers = {
             provider: {
-                azurerm: {
-                    version: "~> 1.41"
-                },
-                null: {
-                    version: "~> 2.1"
-                },
-                local: {
-                    version: "~> 1.4"
-                },
-                kubernetes: {
-                    version: "~> 1.11",
-                    load_config_file: false,
+                azurerm: coreProviders.azurerm(),
+                null: coreProviders.null(),
+                local: coreProviders.local(),
+                kubernetes: coreProviders.kubernetes({
+                    // load_config_file: false,
                     host: terraform.toVariable(`module.${answers.name}-kubernetes.kube_config.0.host`),
                     client_certificate: terraform.toVariable(`base64decode(module.${answers.name}-kubernetes.kube_config.0.client_certificate)`),
                     client_key: terraform.toVariable(`base64decode(module.${answers.name}-kubernetes.kube_config.0.client_key)`),
                     cluster_ca_certificate: terraform.toVariable(`base64decode(module.${answers.name}-kubernetes.kube_config.0.cluster_ca_certificate)`)
-                }
+                })
             }
         }
 
