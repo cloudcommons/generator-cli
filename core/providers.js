44c98255cb146545c84de8b04543e019c5a03935
Sergio Sisternes <sesispla@outlook.com>
2020-02-28 17:12:49 +0000

    Added a centralised providers source for all the generators

---
+++ core/providers.js
@@ -0,0 +1,30 @@
+module.exports.kubernetes = function (config = null) {
+    var provider = {
+        version: "= 1.10"
+    };
+
+    if (config != null) {
+        config = Object.assign(provider, config);
+    }
+
+    return config;
+}
+
+module.exports.azurerm = function () {
+    return {
+        version: "~> 2.0",
+        features: {}
+    };
+}
+
+module.exports.null = function () {
+    return {
+        version: "~> 2.1"
+    };
+}
+
+module.exports.local = function () {
+    return {
+        version: "~> 1.4"
+    };
+}
