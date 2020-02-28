b4d4dfba577bb98b34ae2ffbda1a15d0812b9782
Sergio Sisternes <sesispla@outlook.com>
2020-02-28 17:13:10 +0000

    Updated to ask module v0.2.0

--- generators/azure-aks/templates/aks.tf
+++ generators/azure-aks/templates/aks.tf
@@ -4,7 +4,7 @@ locals {
 
 module "<%= name %>-kubernetes" {
   source                    = "cloudcommons/kubernetes/azure"
-  version                   = "0.1.7"
+  version                   = "0.2.0"
   name                      = local.aks_name
   location                  = var.LOCATION
   resource_group            = <%= resourceGroupReference %>
