ada3c5beed4ab582261b50c88fbdc97eee1e3b15
Sergio Sisternes <sesispla@outlook.com>
2020-02-28 17:14:00 +0000

    Changing back the main menu to list, instead of choice

--- generators/app/questions.js
+++ generators/app/questions.js
@@ -4,7 +4,7 @@ module.exports = function (generator) {
     var questions = [];
 
     questions.push({
-        type: "checkbox",
+        type: "list",
         name: "subGenerators",
         message: "What do you want to create?",
         choices: generators
