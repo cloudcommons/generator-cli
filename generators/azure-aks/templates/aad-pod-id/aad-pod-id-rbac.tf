fbcd1e40918118b04d7d1b56b36d01cd39158b58
Sergio Sisternes <sesispla@outlook.com>
2020-02-29 11:02:32 +0000

    #11 Add aad-pod-id-rbac support

---
+++ generators/azure-aks/templates/aad-pod-id/aad-pod-id-rbac.tf
@@ -0,0 +1,4 @@
+module "aad-pod-identity-rbac" {
+  source  = "cloudcommons/aad-pod-identity-rbac/kubernetes"
+  version = "0.1.0"
+}
---
+++ generators/azure-aks/templates/aad-pod-id/aad-pod-id.tf
@@ -0,0 +1,4 @@
+module "aad-pod-identity" {
+  source  = "cloudcommons/aad-pod-identity/kubernetes"
+  version = "0.1.0"
+}
