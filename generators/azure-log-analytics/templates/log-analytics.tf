29f7e326883dfe477b2331867de644649bb7fa05
Sergio Sisternes <sesispla@outlook.com>
2020-03-08 22:24:35 +0000

    Fixed typo in log analytics

--- generators/azure-log-analytics/templates/log-analytics.tf
+++ generators/azure-log-analytics/templates/log-analytics.tf
@@ -3,7 +3,7 @@ resource "azurerm_log_analytics_workspace" "<%= name %>" {
   location            = var.LOCATION
   resource_group_name = <%= resourceGroupReference %>
   sku                 = "PerGB2018"
-  retention_in_days   = var.LOG_ANALYTICS_RENTION_DAYS
+  retention_in_days   = var.LOG_ANALYTICS_RETENTION_DAYS
 
   tags = {
     app         = var.APP
